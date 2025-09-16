9679a8d047b2ca4129885fe5e2292652f44b3e4e
Muhammad Umer <mumer.29199@gmail.com>
2025-09-16 23:45:18 +0000

    Workshop1

---
+++ hello.py
@@ -0,0 +1,19 @@
+number = 37
+pi: float = 3.1415 #type annotation
+sentence = "the quick brown fox jumped over the lazy fox"
+
+print(number)
+print(type(pi))
+print(sentence)
+print("the valu of pi is:", number, "and the sentence is:", sentence)
+
+# comment
+"""long
+        comment"""
+
+x = 10     # x is an object of type int with a value of 10
+y = x      # y points to the same object as x (see id(x))
+y = 20     # y gets assigned a new int object with a value of 20
+ 
+print("The value of x is: ", x)
+print("The value of y is: ", y)
