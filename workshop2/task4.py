3dd1f4e165e988f1c5973186f0e0490aed86d2a4
Muhammad Umer <mumer.29199@gmail.com>
2025-09-24 03:57:13 +0000

    Task4 Completed

---
+++ workshop2/task4.py
@@ -0,0 +1,16 @@
+def show_arg(func):
+    def wrapper(*args, **kwargs):
+        print("The given arguments are:")
+        print(*args)   
+        return func(*args, **kwargs)
+    return wrapper
+
+@show_arg
+def calculate_sum(*args):
+    total = sum(args)
+    print(f"The sum is: {total}")
+    return total
+
+# Test
+calculate_sum(1, 2, 3)
+calculate_sum(10, 20, 30, 40)
